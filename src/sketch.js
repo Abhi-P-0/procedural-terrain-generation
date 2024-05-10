32f14c0899ec6e35b4ba8dc58eb03894ce6ecf31
Abhijeet Prajapati <abhijeetprajapati85@gmail.com>
2024-05-10 16:26:42 -0400

    added some comments to better explain things

--- src/sketch.js
+++ src/sketch.js
@@ -7,15 +7,18 @@ function setup() {
     createCanvas(width, height);
     background(200);
     noLoop();
+    frameRate(10);
 }
 
 
 function draw() {
     for (let x = 0; x < width; x++) {
         for (let y = 0; y < height; y++) {
-            const noiseVal = noise(x / zoomFactor, y / zoomFactor);
+            const noiseVal = noise(x / zoomFactor, y / zoomFactor); // generates a value between 0 - 1 with a zoomfactor that "narrows" where we are looking (closer pixels i guess is another way of describing)
+
+            // console.log(noiseVal);
             
-            set(x, y, color(255 * noiseVal));
+            set(x, y, color(255 * noiseVal)); // multiplies the noise value by 255 to get grayscale colour and sets it at (x, y) pixel
 
         }
 
