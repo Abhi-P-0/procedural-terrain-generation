439d8a26cc933cf104218275c593e58e3dd3620f
Abhijeet Prajapati <abhijeetprajapati85@gmail.com>
2024-05-10 16:13:42 -0400

    setup canvas to create perlin noise

--- src/sketch.js
+++ src/sketch.js
@@ -1,10 +1,24 @@
 
+let width = 600;
+let height = 600;
+
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(width, height);
+    background(200);
+    noLoop();
 }
 
 
 function draw() {
-    background(0);
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+            const noiseVal = noise(x, y);
+            set(x, y, color(255 * noiseVal));
+        }
+
+    }
+
+    updatePixels();
+
 }
 
