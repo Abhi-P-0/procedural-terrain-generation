1a1e63576dd47db9be5eb70d80f8dd614a6312e2
Abhijeet Prajapati <abhijeetprajapati85@gmail.com>
2024-05-10 16:18:43 -0400

    added a val to zoom in by on perlin noise

--- src/sketch.js
+++ src/sketch.js
@@ -1,6 +1,7 @@
 
 let width = 600;
 let height = 600;
+let zoomFactor = 100;
 
 function setup() {
     createCanvas(width, height);
@@ -12,8 +13,10 @@ function setup() {
 function draw() {
     for (let x = 0; x < width; x++) {
         for (let y = 0; y < height; y++) {
-            const noiseVal = noise(x, y);
+            const noiseVal = noise(x / zoomFactor, y / zoomFactor);
+            
             set(x, y, color(255 * noiseVal));
+
         }
 
     }
